ed6d2ae0a17c5d761c575f6533671649254c45db
arafat2020 <arafatmannan2019@gmail.com>
2021-10-16 00:28:18 +0600

    ch

--- moment/settings.py
+++ moment/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
@@ -142,7 +143,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = [
     BASE_DIR/'static',
-    BASE_DIR/'frontend/static'
+    BASE_DIR/'frontend/static',
+    BASE_DIR/'staticfiles'
 ]
 MEDIA_ROOT = BASE_DIR/'media'
 
